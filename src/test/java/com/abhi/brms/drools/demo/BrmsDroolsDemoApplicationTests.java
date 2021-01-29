5ab8c7f87c701e8f2e5649cd482469ddcad67f16
Kar, Abhishek (ak8073) <ak8073@att.com>
2021-01-28 20:35:59 -0500

    Initial Commit

---
+++ src/test/java/com/abhi/brms/drools/demo/BrmsDroolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.brms.drools.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrmsDroolsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
